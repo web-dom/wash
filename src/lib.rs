3f361c2c954ac2fa26984575f2cd91607553efec
Richard <Richard>
2019-03-22 23:38:52 +0000

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use web_dom::*;
+
+const OP_SYSTEM: i32 = 0;
+const SUBOP_INITIALIZATION: i32 = 0;
+const SUBOP_SPAWN: i32 = 1;
+
+#[no_mangle]
+pub fn sys_call_handler(op: i32, sub_op: i32, param_a: i32, param_b: i32, param_c: i32, param_d: i32) -> () {
+    if op == OP_SYSTEM {
+        if sub_op == SUBOP_INITIALIZATION {
+            element::set_inner_html(param_a, "terminal goes here");
+            sys_call(OP_SYSTEM, SUBOP_SPAWN, to_cstring("helloworld.wasm"), 0, 0, 0);
+        }
+    }
+}
+
+#[no_mangle]
+pub fn main() -> () {
+    sys_call(OP_SYSTEM, SUBOP_INITIALIZATION, 0, 0, 0, 0);
+}
---
+++ wash.js
@@ -0,0 +1,48 @@
+const OP_SYSTEM = 0;
+const SUBOP_INITIALIZATION = 0;
+const SUBOP_SPAWN = 1;
+
+class WasmShell extends HTMLElement {
+  connectedCallback(){
+    let mod = this.getAttribute("module");
+    this.next_pid = 0;
+    this.processes = {};
+    this.spawn(mod==null?"wash.wasm":mod);
+  }
+
+  spawn(wasmSrc){
+    let pid = this.next_pid;
+    this.next_pid += 1;
+    let component = this;
+    this.processes[pid] = WebDOM.run(wasmSrc,{
+      global_sys_call:function(op,subOp,paramA,paramB,paramC,paramD){
+        if(op == OP_SYSTEM){
+          if(subOp == SUBOP_INITIALIZATION){
+            if(pid != 0){
+              throw new Error("only the first module can initialize");
+            }
+            let el = this.env.allocator().a(component);
+            this.exports.sys_call_handler(0,0,el,pid,0,0);
+            return 0
+          }
+          else if(subOp == SUBOP_SPAWN){
+            if(pid != 0){
+              throw new Error("only the first module can spawn");
+            }
+            component.spawn(this.readStringFromMemory(paramA))
+            return 0;
+          }
+          else {
+            if(pid != 0 && paramA != 0){
+              throw new Error("no support for cross process talk right now")
+            } else {
+              return components.processes[0].exports.sys_call_handler(op,subOp,pid,paramB,paramC,paramD)
+            }
+          }
+        }
+      }
+    })
+  }
+}
+
+customElements.define('wasm-shell', WasmShell);

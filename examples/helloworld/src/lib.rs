58126a0e13bf11c4968f17fc96edcbce8e55e828
Richard <Richard>
2019-03-23 00:31:13 +0000

    hello world

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -4,4 +4,10 @@ use wash_syscall::*;
 #[no_mangle]
 pub fn main() -> () {
     sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,72,0,0);
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,69,0,0);
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,76,0,0);
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,76,0,0);
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,79,0,0);
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,33,0,0);
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_FLUSH,WASH,0,0,0);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ extern crate ref_thread_local;
 
 #[derive(Default)]
 struct Shell {
-    component: DOMReference
+    component: DOMReference,
+    stdout: Vec<u8>
 }
 
 ref_thread_local! {
@@ -17,10 +18,15 @@ impl Shell {
     fn handle_sys_call(&mut self,op: i32, sub_op: i32, param_a: i32, param_b: i32, param_c: i32, param_d: i32) -> i32 {
         if op == OP_SYSTEM {
             if sub_op == SUBOP_INITIALIZATION {
-                element::set_inner_html(param_a, "terminal goes here");
+                //setup
             } else if sub_op == SUBOP_STDOUT_PUTC {
-                let c = (param_b as u8) as char;
-                element::set_inner_html(self.component, &c.to_string());
+                self.stdout.push(param_b as u8);
+            } else if sub_op == SUBOP_STDOUT_FLUSH {
+                for i in 0..self.stdout.len() {
+                    let c = self.stdout[i] as char;
+                    element::set_inner_html(self.component, &format!("{}{}",element::get_inner_html(self.component),&c.to_string()));
+                }
+                self.stdout = vec![];
             } else {
                 console::error(&format!("unknown system call: {} {} {} {} {} {}",op,sub_op,param_a,param_b,param_c,param_d));
             }

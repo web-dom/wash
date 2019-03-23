e714d44d0b822b705f62632d26372c8c684d53db
Richard <Richard>
2019-03-23 06:33:48 +0000

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
 use core::str::from_utf8;
-use web_dom::*;
-use wash_syscall::*;
 use ref_thread_local::RefThreadLocal;
+use std::collections::HashMap;
+use wash_syscall::*;
+use web_dom::*;
 #[macro_use]
 extern crate ref_thread_local;
 
@@ -18,10 +18,10 @@ struct Shell {
     characters: Vec<u8>,
     width: usize,
     height: usize,
-    pos:usize,
+    pos: usize,
     key_down_listener: EventListener,
     command: Vec<u8>,
-    known_commands: HashMap<String,String>
+    known_commands: HashMap<String, String>,
 }
 
 ref_thread_local! {
@@ -29,20 +29,32 @@ ref_thread_local! {
 }
 
 impl Shell {
-    fn handle_sys_call(&mut self,op: i32, sub_op: i32, param_a: i32, param_b: i32, param_c: i32, param_d: i32) -> i32 {
+    fn handle_sys_call(
+        &mut self,
+        op: i32,
+        sub_op: i32,
+        param_a: i32,
+        param_b: i32,
+        param_c: i32,
+        param_d: i32,
+    ) -> i32 {
         if op == OP_SYSTEM {
             if sub_op == SUBOP_INITIALIZATION {
                 self.width = 60;
                 self.height = 40;
-                self.characters = vec![32; self.width*self.height];
+                self.characters = vec![32; self.width * self.height];
                 self.component = param_a;
                 self.window = window();
                 self.document = window::get_document(self.window);
-                self.canvas = document::create_element(self.document,"canvas",UNDEFINED);
-                element::set_attribute(self.canvas,"style",r#"image-rendering: -moz-crisp-edges;
+                self.canvas = document::create_element(self.document, "canvas", UNDEFINED);
+                element::set_attribute(
+                    self.canvas,
+                    "style",
+                    r#"image-rendering: -moz-crisp-edges;
   image-rendering: -webkit-crisp-edges;
   image-rendering: pixelated;
-  image-rendering: crisp-edges;"#);
+  image-rendering: crisp-edges;"#,
+                );
                 element::set_attribute(self.canvas, "width", "800");
                 element::set_attribute(self.canvas, "height", "600");
                 self.shadow = customelement::attach_shadow(self.component);
@@ -51,7 +63,7 @@ impl Shell {
                 drawing::set_fill_style(self.ctx, "black");
                 drawing::fill_rect(self.ctx, 0.0, 0.0, 800.0, 600.0);
                 self.key_down_listener = create_event_listener();
-                eventtarget::add_event_listener(self.document,"keydown",self.key_down_listener);
+                eventtarget::add_event_listener(self.document, "keydown", self.key_down_listener);
                 self.print("welcome to WASH, type \"help\" to see a list of commands\n");
                 self.characters[self.pos] = 124;
                 self.render();
@@ -62,7 +74,10 @@ impl Shell {
                         if i != 0 {
                             el = element::get_next_element_sibling(el);
                         }
-                        self.known_commands.insert(element::get_attribute(el,"name"),element::get_attribute(el,"module"));
+                        self.known_commands.insert(
+                            element::get_attribute(el, "name"),
+                            element::get_attribute(el, "module"),
+                        );
                     }
                 }
             } else if sub_op == SUBOP_STDOUT_PUTC {
@@ -74,19 +89,22 @@ impl Shell {
                 self.render();
                 self.stdout = vec![];
             } else {
-                console::error(&format!("unknown system call: {} {} {} {} {} {}",op,sub_op,param_a,param_b,param_c,param_d));
+                console::error(&format!(
+                    "unknown system call: {} {} {} {} {} {}",
+                    op, sub_op, param_a, param_b, param_c, param_d
+                ));
             }
         }
         0
     }
 
-    fn print(&mut self,s:&str){
-        let cs:Vec<char> = s.chars().collect();
-        for i in 0..cs.len(){
+    fn print(&mut self, s: &str) {
+        let cs: Vec<char> = s.chars().collect();
+        for i in 0..cs.len() {
             let c = cs[i] as u8;
             self.characters[self.pos] = c;
             if c == 10 {
-                self.pos = (self.pos/self.width+1)*self.width;
+                self.pos = (self.pos / self.width + 1) * self.width;
             } else {
                 self.pos += 1;
             }
@@ -94,10 +112,10 @@ impl Shell {
         self.render();
     }
 
-    fn handle_event(&mut self, listener:EventListener, event:Event){
+    fn handle_event(&mut self, listener: EventListener, event: Event) {
         if listener == self.key_down_listener {
             let key = keyboardevent::get_key(event);
-            let key_chars:Vec<char> = key.chars().collect();
+            let key_chars: Vec<char> = key.chars().collect();
             if key.len() == 1 {
                 let key_code = key_chars[0] as u8;
                 self.process_char(key_code);
@@ -106,27 +124,27 @@ impl Shell {
                 self.process_char(8);
             } else if key == "Enter" {
                 self.process_char(13);
-            }else {
+            } else {
                 return;
             }
             self.render();
         }
     }
 
-    fn execute(&mut self){
+    fn execute(&mut self) {
         let s = from_utf8(&self.command).unwrap();
         if s == "help" {
-            let keys:Vec<String> = self.known_commands.keys().map(|x|x.clone()).collect();
+            let keys: Vec<String> = self.known_commands.keys().map(|x| x.clone()).collect();
             if keys.len() == 0 {
-               self.print("embarassing...there doesn't seem to be any commands\n");
-               self.characters[self.pos] = 124;
-               self.render();
-           } else {
-               self.print(&keys.join(" "));
-               self.print("\n");
-               self.characters[self.pos] = 124;
-               self.render();
-           }
+                self.print("embarassing...there doesn't seem to be any commands\n");
+                self.characters[self.pos] = 124;
+                self.render();
+            } else {
+                self.print(&keys.join(" "));
+                self.print("\n");
+                self.characters[self.pos] = 124;
+                self.render();
+            }
         } else {
             let cmd = self.known_commands.get(s);
             if cmd.is_some() {
@@ -140,17 +158,17 @@ impl Shell {
         self.command = vec![];
     }
 
-    fn process_char(&mut self, k:u8){
+    fn process_char(&mut self, k: u8) {
         if k == 13 {
             self.characters[self.pos] = 32;
-            self.pos = (self.pos/self.width+1)*self.width;
+            self.pos = (self.pos / self.width + 1) * self.width;
             self.characters[self.pos] = 124;
             self.execute();
             return;
         }
         if k == 10 {
             self.characters[self.pos] = 32;
-            self.pos = (self.pos/self.width+1)*self.width;
+            self.pos = (self.pos / self.width + 1) * self.width;
             self.characters[self.pos] = 124;
             return;
         }
@@ -160,7 +178,7 @@ impl Shell {
             }
             self.command.pop();
             self.characters[self.pos] = 32;
-            self.pos -=1;
+            self.pos -= 1;
             self.characters[self.pos] = 124;
             return;
         }
@@ -169,28 +187,42 @@ impl Shell {
         self.characters[self.pos] = 124;
     }
 
-    fn render(&self){
+    fn render(&self) {
         drawing::set_fill_style(self.ctx, "black");
         drawing::fill_rect(self.ctx, 0.0, 0.0, 800.0, 600.0);
         drawing::set_fill_style(self.ctx, "white");
         drawing::set_font(self.ctx, "18px monospace");
         for x in 0..self.width {
             for y in 0..self.height {
-                drawing::fill_text(self.ctx,&(self.characters[y*self.width+x] as char).to_string(), 0.0+(800.0/self.width as f32)*x as f32, 15.0+(600.0/self.height as f32)*y as f32,1000.0);
+                drawing::fill_text(
+                    self.ctx,
+                    &(self.characters[y * self.width + x] as char).to_string(),
+                    0.0 + (800.0 / self.width as f32) * x as f32,
+                    15.0 + (600.0 / self.height as f32) * y as f32,
+                    1000.0,
+                );
             }
         }
-
     }
 }
 
 #[no_mangle]
-pub fn callback(listener:EventListener,event:Event) -> () {
-    SHELL.borrow_mut().handle_event(listener,event);
+pub fn callback(listener: EventListener, event: Event) -> () {
+    SHELL.borrow_mut().handle_event(listener, event);
 }
 
 #[no_mangle]
-pub fn sys_call_handler(op: i32, sub_op: i32, param_a: i32, param_b: i32, param_c: i32, param_d: i32) -> i32 {
-    SHELL.borrow_mut().handle_sys_call(op,sub_op,param_a,param_b,param_c,param_d)
+pub fn sys_call_handler(
+    op: i32,
+    sub_op: i32,
+    param_a: i32,
+    param_b: i32,
+    param_c: i32,
+    param_d: i32,
+) -> i32 {
+    SHELL
+        .borrow_mut()
+        .handle_sys_call(op, sub_op, param_a, param_b, param_c, param_d)
 }
 
 #[no_mangle]

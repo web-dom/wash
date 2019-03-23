16594ee42b99f1409a8450d63747fd0a7cfd383f
Richard <Richard>
2019-03-23 06:17:01 +0000

    simpler binding

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,15 +1,6 @@
-use web_dom::*;
 use wash_syscall::*;
 
 #[no_mangle]
 pub fn main() -> () {
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,72,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,69,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,76,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,76,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,79,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,33,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,33,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,10,0,0);
-    sys_call(OP_SYSTEM,SUBOP_STDOUT_FLUSH,WASH,0,0,0);
+    print("hello world!\n");
 }
--- wash-syscall/src/lib.rs
+++ wash-syscall/src/lib.rs
@@ -6,3 +6,34 @@ pub const SUBOP_STDOUT_PUTC: i32 = 2;
 pub const SUBOP_STDOUT_FLUSH: i32 = 3;
 pub const SUBOP_STDIN_PUTC: i32 = 4;
 pub const SUBOP_STDIN_FLUSH: i32 = 5;
+
+extern "C" {
+    fn global_sys_call(
+        op: i32,
+        sub_op: i32,
+        param_a: i32,
+        param_b: i32,
+        param_c: i32,
+        param_d: i32,
+    ) -> i32;
+}
+
+pub fn sys_call(
+    op: i32,
+    sub_op: i32,
+    param_a: i32,
+    param_b: i32,
+    param_c: i32,
+    param_d: i32,
+) -> i32 {
+    unsafe { global_sys_call(op, sub_op, param_a, param_b, param_c, param_d) }
+}
+
+pub fn print(s:&str){
+    let cs:Vec<char> = s.chars().collect();
+    for i in 0..cs.len(){
+        let c = cs[i] as i32;
+        sys_call(OP_SYSTEM,SUBOP_STDOUT_PUTC,WASH,c,0,0);
+    }
+    sys_call(OP_SYSTEM,SUBOP_STDOUT_FLUSH,WASH,0,0,0);
+}

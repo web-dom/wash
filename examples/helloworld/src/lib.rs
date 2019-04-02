53a9e1d7e38b00c278ddc9ca35a5d60e1a3955b3
Richard <Richard>
2019-04-02 00:10:10 +0000

    simplest

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,14 +1,11 @@
-extern crate malloc;
 use joss;
-use serde_json::{from_str, json};
 
 #[no_mangle]
 pub fn main() -> () {
     // write to stdout
-    let output_json = json!({
+    joss::syscall(r#"{
         "operation": "write_file",
         "file_descriptor": 1,
         "text":"Hello World!"
-    });
-    joss::syscall(output_json.to_string());
+    }"#.to_owned());
 }
